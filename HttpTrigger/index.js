24c8d04003a09909644a5f9385e5da65a01e5b72
Emmit Kadayifci <emmit@opensoft.com.au>
2019-01-30 23:46:40 +1100

    Document processing steps

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,4 +1,12 @@
 module.exports = async function (context, req) {
+    /* Steps:
+       1 - Call a rest API to Cliq to get the data - timed ~11pm or 5am (ie enought time to send a msg and notify a user to charge the key)
+       2 - Store data in a local database - key date
+       3 - Filter data to identify users with 1 day expiry (possibly add to queue for processing)
+       4 - Call Messagenet API to send sms's
+       5 - Keep logs of all messages for audit purposes
+       6 - Send Admin group a status message (ie no sms sent + email of users/or nightly API call was not successful)
+    */
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
