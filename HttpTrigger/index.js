e1b141f0dd79cc3dd3863d725981386d344952a9
Emmit Kadayifci <ekadayifci@ausgrid.com.au>
2019-02-06 16:50:17 +1100

    Add Package

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -8,10 +8,13 @@ module.exports = async function (context, req) {
        6 - Send Admin group a status message (ie no sms sent + email of users/or nightly API call was not successful)
     */
     context.log('JavaScript HTTP trigger function processed a request.');
-/*
-    var Request = require("request"); 
 
-    Request.post({
+    var request = require("request"); 
+
+
+    getCliqData();
+
+    request.post({
         "headers": { "content-type": "application/json" },
         "url": "http://httpbin.org/post", // <-- Update url
         "body": JSON.stringify({
@@ -23,11 +26,11 @@ module.exports = async function (context, req) {
             return console.dir(error);
         }
         console.dir(JSON.parse(body));
-    });
-*/
-    if (req.query.name || (req.body && req.body.name)) {
+    }); 
+
+/*    if (req.query.name || (req.body && req.body.name)) {
         context.res = {
-            // status: 200, /* Defaults to 200 */
+            // status: 200, 
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
@@ -37,4 +40,56 @@ module.exports = async function (context, req) {
             body: "Please pass a name on the query string or in the request body"
         };
     }
+*/    
 };
+
+// wrap a request in an promise
+/*
+function callCliqWS(url) {  
+    return new Promise((resolve, reject) => {
+
+
+        request(url, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200) {
+                reject('Invalid status code <' + response.statusCode + '>');
+            }
+            resolve(body);
+        });
+    });
+}
+*/
+/*    Request.post({
+        "headers": { "content-type": "application/json" },
+        "url": "http://httpbin.org/post", // <-- Update url
+        "body": JSON.stringify({
+            "firstname": "Nic",
+            "lastname": "Raboy"
+        })
+    }, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        console.dir(JSON.parse(body));
+    }); 
+*/
+
+
+// now to program the "usual" way
+// all you need to do is use async functions and await
+// for functions returning promises
+/*
+async function getCliqData() {
+    try {
+        const html = await callCliqWS('https://microsoft.com')
+        console.log('SHOULD WORK:');
+        console.log(html);
+
+        // try downloading an invalid url
+        await callCliqWS('http://      .com')
+    } catch (error) {
+        console.error('ERROR:');
+        console.error(error);
+    }
+}
+*/
