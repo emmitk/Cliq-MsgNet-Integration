ba94bc5dca43bd64fcc2b21343d28dad51fee6e3
Emmit Kadayifci <ekadayifci@ausgrid.com.au>
2019-03-18 12:26:16 +1100

    changes

--- HttpTrigger_with_logging/index.js
+++ HttpTrigger_with_logging/index.js
@@ -7,37 +7,34 @@ module.exports = async function (context, req) {
      5 - Keep logs of all messages for audit purposes
      6 - Send Admin group a status message (ie no sms sent + email of users/or nightly API call was not successful)
   */
-  context.log('JavaScript HTTP trigger function processed a request.');
 
   var request = require("request"); 
   var fs = require('fs')
     , path = require('path');
   var xml2js = require('xml2js');
-    
-
-  var lv_body="init";
-  var pfxFilePath = path.resolve(__dirname, 'ssl/2-000095399-WebService.pfx');
+      
+  var pfxFilePath = path.resolve(__dirname, '../../ssl/2-000095399-WebService.pfx');
   var mypfx = fs.readFileSync(pfxFilePath);
-  var listUsersJSON;
+  var auditLogJSON;
   var gCounts = {
     total:0,
     active:0,
     expiring:0
-    };
+  };
 
   var lv_cliqCert = process.env["CliqCert"];
   try {
-//      const html = await callCliqWS('https://abloycwm001.assaabloy.net/CLIQWebManager/ws/query/v2/?wsdl');
-//      const html = await callCliqWS('https://www.microsoft.com');
-      const listUsers = await callCliqWSPost('https://abloycwm001.assaabloy.net/CLIQWebManager/ws/query/v2/');
-//      context.log(listUsers);
+      context.log("Pre call");
+      const auditLog = await callCliqWSPost('https://abloycwm001.assaabloy.net/CLIQWebManager/ws/query/v2/');
+      context.log(auditLog);
 
       //Convert output to JSON
-      xml2js.parseString(listUsers, function (err, result) {
+      xml2js.parseString(auditLog, function (err, result) {
         if (err) { context.log(err)};
-        listUsersJSON = result;
+        auditLogJSON = result;
       });
-
+      context.log(JSON.stringify(auditLogJSON));
+/* 
       //Should search for this rather than use indexes in case the schema changes
       const arrPersons = listUsersJSON["S:Envelope"]["S:Body"][0]["ns4:getPersonsResponse"][0]["person"];
       arrPersons.forEach(function(person) {
@@ -49,11 +46,26 @@ module.exports = async function (context, req) {
         
       });
 
-
+*/
       context.res = {
           // status: 200, 
           body: "Total:" + gCounts.total + "\nActive:" + gCounts.active
       };
+
+/*
+      if (error free) {
+          context.res = {
+              status: 200, //default
+              body: "Hello " + (req.query.name || req.body.name)
+          };
+      }
+      else {
+          context.res = {
+              status: 400,
+              body: "Please pass a name on the query string or in the request body"
+          };
+      }
+*/            
 
 //        context.bindings.outputQueueItem = "HTML:" ; // Also works with strings
       var l_datetime = Date.now().toString();
@@ -72,19 +84,22 @@ module.exports = async function (context, req) {
       context.bindings.outputTblStatus = [
         {
           PartitionKey: "Status",
-          RowKey: l_datetime + "-Start",
+          RowKey: l_datetime + "-Start Audit Log",
           status: "Started",
           run_date: getDate()
         },
         {
           PartitionKey: "Status",
-          RowKey: l_datetime + "-Finish",
+          RowKey: l_datetime + "-Finish Audit Log",
           status: "Finished",
           run_date: getDate()
         }
       ];
 
-
+  //cliq-siem
+  const auditOnlyJSON = auditLogJSON["S:Envelope"]["S:Body"][0]["ns4:getAuditTrailsResponse"][0]["auditTrails"];
+  context.bindings.outputBlob = {"auditTrails": auditOnlyJSON};
+  //XML => auditLog
 /*
       context.bindings.tableBinding = [];
       for (var i = 1; i < 10; i++) {
@@ -105,7 +120,7 @@ module.exports = async function (context, req) {
   }
   context.log('Post Call.');
   context.done();        
-return;
+
 
 // wrap a request in an promise
 function callCliqWS(url) {  
@@ -132,10 +147,29 @@ function callCliqWS(url) {
 
 // wrap a request in an promise
 function callCliqWSPost(url) {  
-  let envelope = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://cliq.shared.assaabloy.com/ws/query/v2/">' 
-              + '<soapenv:Header/>'
-              + '<soapenv:Body><v2:getPersons/></soapenv:Body>'
-              + '</soapenv:Envelope>';
+
+  let envelope = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://cliq.shared.assaabloy.com/ws/query/v2/">
+     <soapenv:Header/>
+      <soapenv:Body>
+       <v2:getAuditTrails>
+           <importDateInterval>
+           <from>2018-04-01T00:00+08:00</from>
+           <!--Optional:-->
+           <to>2019-03-01T00:00+08:00</to>
+           <!--You may enter ANY elements at this point-->
+        </importDateInterval>
+        <!--Optional:-->
+        <pagination>
+           <firstResult>0</firstResult>
+           <maxResults>1000</maxResults>
+           <!--You may enter ANY elements at this point-->
+        </pagination>
+       </v2:getAuditTrails>
+      </soapenv:Body>
+    </soapenv:Envelope>`;
+
+  
   var options = {     
     method: 'POST',
     url: url,
@@ -161,62 +195,7 @@ function callCliqWSPost(url) {
       });
   });
 }
-
-
-function callCliqWSPost0(url) {  
-  let soap = require('strong-soap').soap;
-  let xml2js = require('xml2js');
-context.log("In callCliqWSPost");
-  let envelope = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://cliq.shared.assaabloy.com/ws/query/v2/">' 
-              + '<soapenv:Header/>'
-              + '<soapenv:Body><v2:getPersons/></soapenv:Body>'
-              + '</soapenv:Envelope>';
-
- let url_wsdl = 'https://abloycwm001.assaabloy.net/CLIQWebManager/ws/query/v2/?wsdl';
-  /*var parser = new xml2js.Parser();
-   parser.parseString(envelope, {trim: true}, function (err, result) {
-       context.log("Result XML:" + result);
-});*/
-  var options = {     
-    method: 'POST',
-//    url: url,
-    headers: { 
-      "content-type": "text/xml;charset=UTF-8",
- //     "SOAPAction": "https://abloycwm001.assaabloy.net:443/CLIQWebManager/ws/query/v2/",
-      "SOAPAction": "",
-      "Accept-Encoding": "gzip,deflate",
-      "Connection": "Keep-Alive"
-    },
-/*    wsdl_options: {
-      forever: true,
-      rejectUnauthorized: false,
-      strictSSL: false,
-      pfx: mypfx,
-      passphrase: 'e/*hX5' //<= move to keyvault
-    },    */
-    agentOptions: {
-      pfx: mypfx,
-      passphrase: 'e/*hX5' //<= move to keyvault
-    }
-    //body: envelope   
-  };
-
-    return new Promise((resolve, reject) => {
-      soap.createClient(url_wsdl, options, function(err, client) {
-      context.log('Soap client created');
-      context.log('Soap client created:Err:'+ err.code + err + "\nClient:" + client);
-//      var method = client.getPersons(args, function(err, result, envelope, options) {
-//            context.log("result=" + result + err);
-            resolve(client);
- //       });
-      });    
-      context.log('End: New Soap Request');
-// resolve("test");
-      context.log('End: New Soap Request resolved');
-    });      
-}
-
-
+ 
 
 function getDate()
 {
@@ -234,5 +213,6 @@ if (mm < 10) {
 var today = dd + '/' + mm + '/' + yyyy;
 return today;
 } 
+
 
 };
